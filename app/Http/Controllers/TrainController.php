9027d6143925e88aa4776f99dab26c3eadb3741c
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-11 14:51:32 +0200

    add: trains.blade.php and TrainController

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TrainController extends Controller
+{
+    public function index() {
+        return view('trains');
+    }
+}
