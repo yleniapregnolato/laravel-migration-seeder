f5e3c673d601e7b23d71e614cbe7cb25c22663a6
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-11 14:39:19 +0200

    feat: description in create table trains

--- database/migrations/2024_06_11_121219_create_trains_table.php
+++ database/migrations/2024_06_11_121219_create_trains_table.php
@@ -10,15 +10,20 @@ return new class extends Migration
      * Run the migrations.
      * Create TABLE 'trains' (
      *              'id' UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     *              'azienda' VARCHAR(255) NOT NULL,
-     *              'stazione
+     *              'azienda' VARCHAR(255) NULL,
+     *              'stazione_arrivo' TIME NOT NULL,
+     *              'orario_partenza' TIME NOT NULL,
+     *              'codice_treno' VARCHAR(255) NOT NULL,
+     *              'numero_carrozze' UNSIGNET TINYINT NOT NULL,
+     *              'in_orario' TINYINT NOT NULL,
+     *              'cancellato' TINYINT NOT NULL
      * )
      */
     public function up(): void
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->string('azienda');
+            $table->string('azienda')->nullable();
             $table->string('stazione_arrivo');
             $table->time('orario_partenza');
             $table->time('orario_arrivo');
