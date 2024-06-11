48c1c1af4295c073092ef035c27fe576c7ea014e
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-11 15:05:24 +0200

    feat: print trains in trains.blade.php

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
     public function index() {
-        return view('trains');
+        $trains = Train::all();
+        return view('trains', compact('trains'));
     }
 }
--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Lista di treni</h1>
+<h1>TRENI:</h1>
+
+
+    @foreach ($trains as $train)
+        <ul>
+            <div class="fw-bold">Azienda: {{ $train->azienda }}</div> 
+            <li>
+                Stazione di arrivo: {{ $train->stazione_arrivo }}
+            </li>
+            <li>
+                Orario di partenza: {{ $train->orario_partenza }}
+            </li>
+            <li>
+                Orario di arrivo: {{ $train->orario_arrivo }}
+            </li>
+            <li>
+                Codice treno: {{ $train->codice_treno }}
+            </li>
+            <li>
+                Numero di carrozze: {{ $train->numero_carrozze }}
+            </li>
+            <li>
+                In orario: {{ $train->in_orario }}
+            </li>
+            <li>
+                Cancellato: {{ $train->cancellato }}
+            </li>
+        </ul>
+    @endforeach
+
 @endsection
