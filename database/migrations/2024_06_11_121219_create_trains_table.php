e63bdffac27f3af1efa6a68a230ff20bbd2c4d85
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-11 14:26:41 +0200

    feat: create_trains_table

---
+++ database/migrations/2024_06_11_121219_create_trains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda');
+            $table->string('stazione di arrivo');
+            $table->time('orario di partenza');
+            $table->time('orario di arrivo');
+            $table->string('codice treno');
+            $table->unsignedTinyInteger('numero carrozze');
+            $table->boolean('in orario');
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
