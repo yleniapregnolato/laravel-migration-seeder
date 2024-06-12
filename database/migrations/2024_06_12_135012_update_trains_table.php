e30b72b3b8455ddf492420ed1b6610f50445ce82
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-12 15:57:32 +0200

    feat: update_trains in migrations

---
+++ database/migrations/2024_06_12_135012_update_trains_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->date('data_partenza')->after('stazione_arrivo');
+            $table->date('data_arrivo')->after('orario_partenza');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('data_partenza');
+            $table->dropColumn('data_arrivo');
+        });
+    }
+};
