55a0f760bf9e1e3efff2bf865161cf07b63a7e8c
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-12 16:57:27 +0200

    feat: printout of trains leaving from today onwards

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -8,10 +8,10 @@ use Illuminate\Http\Request;
 class TrainController extends Controller
 {
     public function index() {
-        // $today = now();
+        $today = now();
 
-        $trains = Train::all();
-        // $trains = Train::where("data_partenza", "=", $today)->get();
+        // $trains = Train::all();
+        $trains = Train::where("data_partenza", ">=", $today)->get();
         return view('trains', compact('trains'));
     }
 }
